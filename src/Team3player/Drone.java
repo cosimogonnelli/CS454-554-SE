a116242921e1ef8b1fc0bceef4c8fce42eefef9c
crenslow <39916623+crenslow@users.noreply.github.com>
2020-11-12 15:53:46 -0800

    updated drones now pick up enemies and drop them in water

--- src/Team3player/Drone.java
+++ src/Team3player/Drone.java
@@ -13,35 +13,38 @@ public class Drone extends Unit {
      *******************************************************/
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        if(HQLocation.size() > 0)
-            goToLocation(HQLocation.get(0));
-        else {
-            goTo(randomDirection());
-        }
+
         Team enemy = rc.getTeam().opponent();
         if (!rc.isCurrentlyHoldingUnit()) {
             // See if there are any enemy robots within capturing range
             RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED,enemy);
-
-            if (robots.length > 0 && robots[0].type != RobotType.DELIVERY_DRONE && robots[0].type != RobotType.FULFILLMENT_CENTER && robots[0].type != RobotType.HQ && robots[0].type != RobotType.DESIGN_SCHOOL && robots[0].type != RobotType.REFINERY) {
+            for (RobotInfo rob: robots
+                 ) {
+                System.out.println(rob.type);
+            }
+            if (robots.length > 0) {
                 // Pick up a first robot within range
-                if(robots[0].type == RobotType.LANDSCAPER || robots[0].type == RobotType.MINER) {
+                if(rc.canPickUpUnit(robots[0].getID())) {
                     rc.pickUpUnit(robots[0].getID());
-                    System.out.println("I picked up " + robots[0].getID() + "!");
+                    //System.out.println("I picked up " + robots[0].getID() + "!");
                 }
             }
         } else {
             // No close robots, so search for robots within sight radius
             for(Direction dir: directions){
-                //if(rc.senseFlooding(rc.adjacentLocation(dir))){
+                if(rc.senseFlooding(rc.adjacentLocation(dir))){
                     if(rc.canDropUnit(dir)) {
                         rc.dropUnit(dir);
-                        System.out.println("I killed a unit!");
+                        //System.out.println("I Destroyed a unit!");
                     }
-                //}
+                }
             }
-            //if(rc.canDropUnit(randomDirection()))
-            //    rc.dropUnit(randomDirection());
+        }
+        if(HQLocation.size() > 0)
+            goToLocation(HQLocation.get(0));
+        else {
+            goTo(randomDirection());
         }
     }
+
 }
